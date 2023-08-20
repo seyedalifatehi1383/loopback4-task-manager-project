24f069cd961cb7dea3e7f458ff62e47efab2101b
Mohammad reza <mamalreza83@gmail.com>
2023-08-20 11:27:05 +0400

    fixing the bug of not reading data

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -11,6 +11,7 @@ import {model, property, repository} from '@loopback/repository';
 import {
   get,
   getModelSchemaRef,
+  HttpErrors,
   post,
   requestBody,
   SchemaObject,
@@ -67,15 +68,18 @@ export class UserController {
     })
     newUserRequest: NewUser
   ): Promise<NewUser> {
-    const password = await hash(newUserRequest.password, await genSalt());
-    newUserRequest.password = password
+    if (newUserRequest.password.length >7 && newUserRequest.email.endsWith('@gmail.com')) {
+      const password = await hash(newUserRequest.password, await genSalt());
+    const savedUser = await this.newUserRepository.create(
+      _.omit(newUserRequest, 'realm'  , 'emailVerified' ,'verificationToken'),
+    );
 
-    // const User = this.userService.convertToUserProfile(newUserRequest as any)
-    // const token = await this.jwtService.generateToken(User)
+    // await this.newUserRepository.userCredentials(savedUser.id).create({password});
 
-    return await this.newUserRepository.create(
-      _.omit(newUserRequest, 'realm' , 'verificationToken' , 'emailVerified')
-    )  ;
+    return savedUser;
+    } else {
+      throw new HttpErrors.Forbidden('email is not valid or password is weak')
+    }
   }
 
 
@@ -110,11 +114,20 @@ export class UserController {
       },
     })
     newUser : NewUser
-  ): Promise<{token: string}> {
-    // const User =  this.userService.convertToUserProfile(newUser as any)
-    // create a JSON Web Token based on the user profile
-    const token = await this.jwtService.generateToken(newUser as any);
-    return {token};
+  ): Promise<any> {
+    // ensure the user exists, and the password is correct
+    const user = await this.newUserRepository.findOne({where : {email : newUser.email}})
+    // convert a User object into a UserProfile object (reduced set of properties)
+if (user === null) {
+  throw new HttpErrors.NotFound('there is no such user')
+} else {
+  const userProfile = this.userService.convertToUserProfile(user);
+  // // create a JSON Web Token based on the user profile
+  const token = await this.jwtService.generateToken(userProfile);
+  // return {token };
+  return token
+
+}
   }
 
 
@@ -134,11 +147,11 @@ export class UserController {
     },
   })
   async whoAmI(
-    @inject(SecurityBindings.USER as any)
+    @inject(SecurityBindings.USER)
     currentUserProfile: UserProfile,
     // newUser : NewUser
   ): Promise<any> {
-    return currentUserProfile.email;
+    return currentUserProfile;
   }
 }
 
--- src/models/new-user.model.ts
+++ src/models/new-user.model.ts
@@ -1,26 +1,26 @@
 import {Entity, model, property} from '@loopback/repository';
 import { User } from "@loopback/authentication-jwt";
 @model()
-export class NewUser extends Entity {
-  @property({
-    type: 'number',
-    id: true,
-    generated: true,
-  })
-  id: number;
+export class NewUser extends User {
+  // @property({
+  //   type: 'number',
+  //   id: true,
+  //   generated: true,
+  // })
+  // id: number;
 
-  @property({
-    type: 'string',
-    required: true,
-  })
-  username: string;
+  // @property({
+  //   type: 'string',
+  //   required: true,
+  // })
+  // username: string;
 
 
-  @property({
-    type: 'string',
-    required: true,
-  })
-  email: string;
+  // @property({
+  //   type: 'string',
+  //   required: true,
+  // })
+  // email: string;
 
   @property({
     type: 'string',
