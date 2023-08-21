4dd3e8d0616b49bfd52c1cda4a68e033151edb34
Mohammad reza <mamalreza83@gmail.com>
2023-08-21 14:29:55 +0400

    comment task controller because tasks add in new user task

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,4 @@
 export * from './ping.controller';
-export * from './task.controller';
+// export * from './task.controller';
 export * from './user.controller';
 export * from './new-user-task.controller';
--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,153 +1,153 @@
-import {
-  Count,
-  CountSchema,
-  Filter,
-  FilterExcludingWhere,
-  repository,
-  Where,
-} from '@loopback/repository';
-import {
-  post,
-  param,
-  get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
-  response,
-} from '@loopback/rest';
-import {Task} from '../models';
-import {TaskRepository} from '../repositories';
-import {authenticate} from '@loopback/authentication';
-// ------------------------------------
-@authenticate('jwt') // <---- Apply the @authenticate decorator at the class level
+// import {
+//   Count,
+//   CountSchema,
+//   Filter,
+//   FilterExcludingWhere,
+//   repository,
+//   Where,
+// } from '@loopback/repository';
+// import {
+//   post,
+//   param,
+//   get,
+//   getModelSchemaRef,
+//   patch,
+//   put,
+//   del,
+//   requestBody,
+//   response,
+// } from '@loopback/rest';
+// import {Task} from '../models';
+// import {TaskRepository} from '../repositories';
+// import {authenticate} from '@loopback/authentication';
+// // ------------------------------------
+// @authenticate('jwt') // <---- Apply the @authenticate decorator at the class level
 
-export class TaskController {
-  constructor(
-    @repository(TaskRepository)
-    public taskRepository : TaskRepository,
-  ) {}
+// export class TaskController {
+//   constructor(
+//     @repository(TaskRepository)
+//     public taskRepository : TaskRepository,
+//   ) {}
 
-  @post('/tasks')
-  @response(200, {
-    description: 'Task model instance',
-    content: {'application/json': {schema: getModelSchemaRef(Task)}},
-  })
-  async create(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Task, {
-            title: 'NewTask',
-            exclude: ['id' ],
-          }),
-        },
-      },
-    })
-    task: Omit<Task, 'id'>,
-  ): Promise<Task> {
-    return this.taskRepository.create(task);
-  }
+//   @post('/tasks')
+//   @response(200, {
+//     description: 'Task model instance',
+//     content: {'application/json': {schema: getModelSchemaRef(Task)}},
+//   })
+//   async create(
+//     @requestBody({
+//       content: {
+//         'application/json': {
+//           schema: getModelSchemaRef(Task, {
+//             title: 'NewTask',
+//             exclude: ['id' ],
+//           }),
+//         },
+//       },
+//     })
+//     task: Omit<Task, 'id'>,
+//   ): Promise<Task> {
+//     return this.taskRepository.create(task);
+//   }
 
-  @get('/tasks/count')
-  @response(200, {
-    description: 'Task model count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async count(
-    @param.where(Task) where?: Where<Task>,
-  ): Promise<Count> {
-    return this.taskRepository.count(where);
-  }
+//   @get('/tasks/count')
+//   @response(200, {
+//     description: 'Task model count',
+//     content: {'application/json': {schema: CountSchema}},
+//   })
+//   async count(
+//     @param.where(Task) where?: Where<Task>,
+//   ): Promise<Count> {
+//     return this.taskRepository.count(where);
+//   }
 
-  @get('/tasks')
-  @response(200, {
-    description: 'Array of Task model instances',
-    content: {
-      'application/json': {
-        schema: {
-          type: 'array',
-          items: getModelSchemaRef(Task, {includeRelations: true}),
-        },
-      },
-    },
-  })
-  async find(
-    @param.filter(Task) filter?: Filter<Task>,
-  ): Promise<Task[]> {
-    return this.taskRepository.find(filter);
-  }
+//   @get('/tasks')
+//   @response(200, {
+//     description: 'Array of Task model instances',
+//     content: {
+//       'application/json': {
+//         schema: {
+//           type: 'array',
+//           items: getModelSchemaRef(Task, {includeRelations: true}),
+//         },
+//       },
+//     },
+//   })
+//   async find(
+//     @param.filter(Task) filter?: Filter<Task>,
+//   ): Promise<Task[]> {
+//     return this.taskRepository.find(filter);
+//   }
 
-  @patch('/tasks')
-  @response(200, {
-    description: 'Task PATCH success count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Task, {partial: true}),
-        },
-      },
-    })
-    task: Task,
-    @param.where(Task) where?: Where<Task>,
-  ): Promise<Count> {
-    return this.taskRepository.updateAll(task, where);
-  }
+//   @patch('/tasks')
+//   @response(200, {
+//     description: 'Task PATCH success count',
+//     content: {'application/json': {schema: CountSchema}},
+//   })
+//   async updateAll(
+//     @requestBody({
+//       content: {
+//         'application/json': {
+//           schema: getModelSchemaRef(Task, {partial: true}),
+//         },
+//       },
+//     })
+//     task: Task,
+//     @param.where(Task) where?: Where<Task>,
+//   ): Promise<Count> {
+//     return this.taskRepository.updateAll(task, where);
+//   }
 
-  @get('/tasks/{id}')
-  @response(200, {
-    description: 'Task model instance',
-    content: {
-      'application/json': {
-        schema: getModelSchemaRef(Task, {includeRelations: true}),
-      },
-    },
-  })
-  async findById(
-    @param.path.number('id') id: number,
-    @param.filter(Task, {exclude: 'where'}) filter?: FilterExcludingWhere<Task>
-  ): Promise<Task> {
-    return this.taskRepository.findById(id, filter);
-  }
+//   @get('/tasks/{id}')
+//   @response(200, {
+//     description: 'Task model instance',
+//     content: {
+//       'application/json': {
+//         schema: getModelSchemaRef(Task, {includeRelations: true}),
+//       },
+//     },
+//   })
+//   async findById(
+//     @param.path.number('id') id: number,
+//     @param.filter(Task, {exclude: 'where'}) filter?: FilterExcludingWhere<Task>
+//   ): Promise<Task> {
+//     return this.taskRepository.findById(id, filter);
+//   }
 
-  @patch('/tasks/{id}')
-  @response(204, {
-    description: 'Task PATCH success',
-  })
-  async updateById(
-    @param.path.number('id') id: number,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Task, {partial: true}),
-        },
-      },
-    })
-    task: Task,
-  ): Promise<void> {
-    await this.taskRepository.updateById(id, task);
-  }
+//   @patch('/tasks/{id}')
+//   @response(204, {
+//     description: 'Task PATCH success',
+//   })
+//   async updateById(
+//     @param.path.number('id') id: number,
+//     @requestBody({
+//       content: {
+//         'application/json': {
+//           schema: getModelSchemaRef(Task, {partial: true}),
+//         },
+//       },
+//     })
+//     task: Task,
+//   ): Promise<void> {
+//     await this.taskRepository.updateById(id, task);
+//   }
 
-  @put('/tasks/{id}')
-  @response(204, {
-    description: 'Task PUT success',
-  })
-  async replaceById(
-    @param.path.number('id') id: number,
-    @requestBody() task: Task,
-  ): Promise<void> {
-    await this.taskRepository.replaceById(id, task);
-  }
+//   @put('/tasks/{id}')
+//   @response(204, {
+//     description: 'Task PUT success',
+//   })
+//   async replaceById(
+//     @param.path.number('id') id: number,
+//     @requestBody() task: Task,
+//   ): Promise<void> {
+//     await this.taskRepository.replaceById(id, task);
+//   }
 
-  @del('/tasks/{id}')
-  @response(204, {
-    description: 'Task DELETE success',
-  })
-  async deleteById(@param.path.number('id') id: number): Promise<void> {
-    await this.taskRepository.deleteById(id);
-  }
-}
+//   @del('/tasks/{id}')
+//   @response(204, {
+//     description: 'Task DELETE success',
+//   })
+//   async deleteById(@param.path.number('id') id: number): Promise<void> {
+//     await this.taskRepository.deleteById(id);
+//   }
+// }
